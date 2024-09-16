5a68b785285948fbd536a78b3c2f3447d17faea9
samuheinonen <samu.heinone@gmail.com>
2024-09-16 10:09:57 +0300

    Luento3 peli

--- react_1/src/App.tsx
+++ react_1/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Layout = styled.div`
+  background: rgb(32, 222, 101);
+  widows: 100vw;
+  height: 100dvh;
+`
+
+const Navigation = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: #22f1e3;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+
+const HomeButton = styled.button`
+  margin-left: 10px;
+  border-radius: 3px;
+`
+const Points = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #d13d9d;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+export default function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Navigation>
+          <HomeButton>KOTI</HomeButton>
+          <Points></Points>
+        </Navigation>
+      </Layout>
     </>
   )
 }
 
-export default App
+
