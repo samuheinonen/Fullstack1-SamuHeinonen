556a27d57a8e9e063f423697a13de3a32ec86408
samuheinonen <samu.heinone@gmail.com>
2024-09-17 08:26:48 +0300

    Koodin viimeistely

--- react_1/src/App.tsx
+++ react_1/src/App.tsx
@@ -28,11 +28,17 @@ const Points = styled.div`
   border-radius: 50%;
 `
 
-function Ball() {
+interface BallProps {
+  maxCount: number
+  x: number
+  y: number
+}
+
+function Ball({maxCount, x, y}: BallProps) {
 
   const [clicked, setClicked] = useState(0)
 
-  const maxCount = 10
+  //const maxCount = 10
 
   const style: CSSProperties = {
     background: "red",
@@ -42,9 +48,10 @@ function Ball() {
     justifyContent: "center",
     alignItems: "center",
     borderRadius: "50%",
-    /* position: "absolute", */
+    position: "absolute",
     userSelect: "none",
-    cursor: "pointer"
+    cursor: "pointer",
+    transform: `translate(${x}px,${y}px)`
   }
 
   if(clicked >= maxCount){
@@ -53,26 +60,53 @@ function Ball() {
 
   return <>
 
-    <div style={style} onClick={()=> setClicked(clicked + 1)}> {clicked} </div>
+    <div style={style} onClick={()=> setClicked(clicked + 1)}> {clicked} / {maxCount} </div>
 
   </>
 }
 
+
+// Tehdään työkalu-funktio: randomInteger()
+// Saa parametrina min ja max arvot
+// Palauttaa satunnaisen kokonaisluvun min-max väliltä
+function randomInteger(min: number, max: number) {
+
+  const random = Math.floor((Math.random() * (max - min)) + min)
+
+  return random
+}
+
 export default function App() {
 
+  // Array(20) luo tyhjän arrayn minkä pituus on 20
+  // .fill täyttää annetulla arvolla kaikki 20 indeksiä
+  // .map käy arrayn jokaisen indeksin läpi ja luo uuden arrayn paluuarvojen perusteella
+  const allBalls = Array(20).fill(null).map((_, i) => {
 
+    return <Ball
+      key={i}
+      maxCount={randomInteger(1, 6)}
+      x={randomInteger(1, window.innerWidth)}
+      y={randomInteger(1, window.innerHeight)}>
+
+    </Ball>
+
+  })
 
   return (
     <>
       <Layout>
         <Navigation>
-          <HomeButton>KOTI</HomeButton>
+          <HomeButton>Koti</HomeButton>
           <Points></Points>
         </Navigation>
-        <Ball></Ball>
+
+        {allBalls}
+
       </Layout>
     </>
   )
 }
+
 
 
